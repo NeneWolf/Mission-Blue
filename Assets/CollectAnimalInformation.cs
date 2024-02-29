f4cf6e3fd1e7c33e7d000a606f3d6a47d51a79c7
NeneWolf <ineslobo@hotmail.com>
2024-02-29 02:21:56 +0000

    Convai Implemented Successfully !

--- Assets/CollectAnimalInformation.cs
+++ Assets/CollectAnimalInformation.cs
@@ -3,34 +3,33 @@ using MalbersAnimations.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CollectAnimalInformation : MonoBehaviour
 {
     [SerializeField] Aim aim;
     [SerializeField] ConvaiTextInOut convaiTrigger;
-    private Transform lastAimTarget;
 
-    private void Start()
+    Transform target;
+    Transform lastAimTarget;
+
+    private void Update()
     {
-        // Subscribe to the AimTargetChanged event to be notified when the AimTarget changes.
-        aim.OnSetTarget.AddListener(OnAimTargetChanged);
+        if(aim.AimTarget != null)
+        {
+            target = aim.AimTarget;
+            
+            if(target != lastAimTarget )
+            {
+                OnAimTargetChanged();
+            }
+
+            lastAimTarget = target;
+        }
     }
 
-    private void OnAimTargetChanged(Transform newAimTarget)
+    private void OnAimTargetChanged()
     {
-        // Collect data or perform actions based on the new AimTarget.
-        //Debug.Log($"AimTarget changed to: {newAimTarget}");
-
-        convaiTrigger.TriggerChatKnowledge(newAimTarget.name);
-
-        //// Example: Check if the AimTarget is different from the last one.
-        //if (newAimTarget != lastAimTarget)
-        //{
-        //    // Collect additional data or perform specific actions.
-        //    Debug.Log("AimTarget has changed.");
-        //}
-
-        //// Update the lastAimTarget for future comparisons.
-        //lastAimTarget = newAimTarget;
+        convaiTrigger.TriggerChatKnowledge(aim.AimTarget.name);
     }
 }
